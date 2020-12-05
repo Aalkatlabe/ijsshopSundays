263342a83e5a2772f49223afc505b2c9e40a86d4
Alaa Alkatlabe <302232189@student.rocmondriaan.nl>
2020-12-06 00:32:40 +0100

    ijsshopSundays toegevoegd

---
+++ Ijs-shop-Sunday/Ijs.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ijs_shop_Sunday
+{
+    class Ijs
+    {
+        public string smaak = "vanille";
+        public string topping = "geen topping";
+        public string hoorn = "middel";
+
+        public void toString()
+        {
+            Console.WriteLine($"Het ijsje heeft de smaak {smaak}, heeft {topping} en heeft een {hoorn} hoorn.");
+        }
+        public void smaakVerandern()
+        {
+            Console.WriteLine("Welke smaak wilt u hebben!");
+            string[] smaken = { "Vanille", "Chocola", "Aardbei" };
+            for (int i = 0; i < 3; i++)
+            {
+                Console.WriteLine($"{i + 1}. {smaken[i]}");
+            }
+            int antwoord = int.Parse(Console.ReadLine());
+            string keuze = smaken[antwoord - 1];
+            smaak = keuze;
+
+        }
+        public void toppingVerandern()
+        {
+            Console.WriteLine("Welke topping wilt u hebben!");
+            string[] toppings = { "banaan", "discodip", "chocoladesaus" };
+            for (int i = 0; i < 3; i++)
+            {
+                Console.WriteLine($"{i + 1}. {toppings[i]}");
+
+            }
+            int antwoord = int.Parse(Console.ReadLine());
+            string keuze = toppings[antwoord - 1];
+            topping = keuze;
+        }
+        public void hoornVerandern()
+        {
+            Console.WriteLine("Welke hoorn wilt u hebben!");
+            string[] hoorns = { "klein", "middel", "groot" };
+            for (int i = 0; i < 3; i++)
+            {
+                Console.WriteLine($"{i + 1}. {hoorns[i]}");
+
+            }
+            int antwoord = int.Parse(Console.ReadLine());
+            string keuze = hoorns[antwoord - 1];
+            hoorn = keuze;
+        }
+    }
+}
---
+++ Ijs-shop-Sunday/Program.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ijs_shop_Sunday
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welkom bij ijsshop: Sundays");
+            Ijs ijs1 = new Ijs();
+            ijs1.toString();
+            bool herhalen = true;
+            while (herhalen)
+            {
+                Console.WriteLine("Wilt u het ijsje veranderen?");
+                Console.WriteLine("kies ja of nee");
+                try
+                {
+                    string keuze = Console.ReadLine();
+                    if (keuze == "ja")
+                    {
+                        Console.WriteLine("Wat wilt u veranderen");
+                        Console.WriteLine("Kies smaak, topping of hoorn");
+                        string[] ijskeuze = { "smaak", "topping", "hoorn" };
+                        for (int i = 0; i < 3; i++)
+                        {
+                            Console.WriteLine($"{i + 1}. {ijskeuze[i]}");
+                        }
+                        int antwoord = int.Parse(Console.ReadLine());
+                        string veranderingKeuze = ijskeuze[antwoord - 1];
+                        if (veranderingKeuze == "smaak")
+                        {
+                            ijs1.smaakVerandern();
+                            ijs1.toString();
+                        }
+                        else if (veranderingKeuze == "topping")
+                        {
+                            ijs1.toppingVerandern();
+                            ijs1.toString();
+                        }
+                        else if (veranderingKeuze == "hoorn")
+                        {
+                            ijs1.hoornVerandern();
+                            ijs1.toString();
+                        }
+                    }
+                    else if (keuze == "nee")
+                    {
+                        Console.WriteLine("Oke, uw bestelling is aan de balie op te halen");
+                        herhalen = false;
+                    }
+                    else
+                    {
+                        Console.WriteLine("je mag alleen ja of nee kiezen");
+                    }
+                }
+                catch (FormatException ex)
+                {
+                    Console.WriteLine("Er is een fout opgetreden. Maak gebruik van het juiste format");
+                }
+                catch (Exception ex)
+                {
+                    Console.WriteLine("Er is een fout opgetreden. De bestelling wordt afgesloten");
+
+                }
+            }
+            Console.ReadLine();
+
+        }
+    }
+}
